5fdb6d5809abb0bcad875cc8e6777775e628171e
Josh-M2 <joshuamagdada@gmail.com>
2025-04-18 18:31:45 +0800

    changed cors config

--- server/src/config/corsConfig.js
+++ server/src/config/corsConfig.js
@@ -1,11 +1,9 @@
 const corsOptions = {
-  // origin: process.env.CLIENT_URL || "http://localhost:5173",
-   origin: "*",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  //origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: false,
 };
 
 export default corsOptions;
-
-
